c545eb3fddfe358ca0da29fc27ddc9949cebc528
Ronak Dumaniya <ronak.dumaniya@gmail.com>
2022-10-19 14:18:14 +0530

    PDF pagesize updated

--- src/app/views/order/order.component.ts
+++ src/app/views/order/order.component.ts
@@ -82,7 +82,7 @@ export class OrderComponent implements OnInit {
       let fileWidth = 210;
       let fileHeight = (canvas.height * fileWidth) / canvas.width;
       const FILEURI = canvas.toDataURL('image/png');
-      let PDF = new jsPDF('p', 'mm', 'a4');
+      let PDF = new jsPDF('p', 'mm', [265, 210]);
       let position = 0;
       PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
       var today = this.dateToYMD(new Date());
