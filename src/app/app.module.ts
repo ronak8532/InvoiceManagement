926363bf8305714ce3cac94a7a34af908584b7ba
Ronak Dumaniya <ronak.dumaniya@gmail.com>
2022-11-27 10:29:52 +0530

    WebApp converted to PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -52,6 +52,8 @@ import { IconModule, IconSetService } from '@coreui/icons-angular';
 import { ToastrModule } from 'ngx-toastr';
 import { PdfTestComponent } from './views/pdf-test/pdf-test.component';
 import { NgxSpinnerModule } from "ngx-spinner";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
   suppressScrollX: true,
 };
@@ -98,7 +100,13 @@ const APP_CONTAINERS = [
     BadgeModule,
     ListGroupModule,
     CardModule,
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     {
