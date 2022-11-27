4ce718b2337119cfab016f57e799cb11a89fcaa9
Ronak Dumaniya <ronak.dumaniya@gmail.com>
2022-11-27 20:01:54 +0530

    Service worker removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -52,7 +52,6 @@ import { IconModule, IconSetService } from '@coreui/icons-angular';
 import { ToastrModule } from 'ngx-toastr';
 import { PdfTestComponent } from './views/pdf-test/pdf-test.component';
 import { NgxSpinnerModule } from "ngx-spinner";
-import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
   suppressScrollX: true,
@@ -100,13 +99,7 @@ const APP_CONTAINERS = [
     BadgeModule,
     ListGroupModule,
     CardModule,
-    NgxSpinnerModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
+    NgxSpinnerModule
   ],
   providers: [
     {
